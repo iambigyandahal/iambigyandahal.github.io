855d70ef7997c439a809d9c304af0d116c2cfb93
Bigyan Dahal <mebigyan01@gmail.com>
2020-01-08 15:16:26 +0545

    Add files via upload

---
+++ otakujin/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+        <meta http-equiv="ScreenOrientation" content="autoRotate:disabled">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>Otakujin</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <div class="main-head">
+                    <img src="naruto.png" class="chibi_naruto">
+                    <img src="SVG.svg" class="brand">
+                    <img src="luffy.png" class="chibi_luffy">
+                </div>
+                <nav>
+                    <div class="upper-nav">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Anime List<br>(A-Z)</a></li>
+                            <li><a href="#">Popular</a></li>
+                        </ul>
+                        <div class="search-div">
+                            <div>
+  								<input type="text" placeholder="Search anime ..." name="search">
+  								<button type="submit"><i class="fa fa-search"></i></button>
+							</div>
+                        </div>
+                    </div>
+                    <ul class="lower-nav">
+                        <li><a href="#">Latest</a></li>
+                        <li><a href="#">Ongoing</a></li>
+                        <li><a href="#">Dubbed</a></li>
+                        <li><a href="#">Upcoming</a></li>
+                        <li><a href="#">Movies</a></li>
+                    </ul>
+                </nav>
+            </header>
+            
+            <div class="content" style="width: 100%;">
+                <figure style="width: 10%;">
+                    <img src="1.jpg" style="width: 100%;">
+                    <figcaption>Anime</figcaption>
+                </figure>
+            </div>
+        </div>
+    </body>
+</html>
