e79d90a3f60af8a76a15af9bb595ed49f91928a7
Bigyan Dahal <mebigyan01@gmail.com>
2020-01-12 22:44:43 +0545

    Delete script.js

--- otakujin/js/script.js
+++
@@ -1,5 +0,0 @@
-function toggleMode() {
-    var container = document.getElementById("container");
-    var containerCurrentClass = container.className;
-    container.className = (containerCurrentClass == "container-lm") ? "container-dm" : "container-lm";
-}
